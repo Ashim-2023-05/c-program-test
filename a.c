854a8592268bfd8866d84c968d720c03f52cb818
Ashim-2023-05 <ashimansary05@gmail.com>
2024-03-18 15:43:09 +0530

    1st commit

--- a.c
+++ a.c
@@ -1,1 +1,15 @@
-//  C File
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void myfunc() {
+    static int x = 5;
+    printf("%d\n", ++x);
+}
+
+int main() {
+    myfunc();
+    myfunc();
+    myfunc();
+    return EXIT_FAILURE;
+}
